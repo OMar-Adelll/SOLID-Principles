6401a5033d4c0d5aa0ada12a0f85f8d83bb78787
Omar Adel <OmarAdel.connect@gmail.com>
2026-02-28 22:03:06 +0200

    Try push from Antigravity

---
+++ Single-Resposibility/code/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+#define ll long long
+#define sz(x) (int)x.size()
+#define nl '\n'
+
+int main() {
+    cout << "Omar Adel" << nl;    
+  return 0;
+}
